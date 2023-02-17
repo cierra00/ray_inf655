ec2e23b23346f1c6488385d00fcabcc81433c734
Cierra Ray <cierraray2@gmail.com>
2023-02-16 19:45:19 -0500

    Props Fixed

--- src/comps/TodoApp.js
+++ src/comps/TodoApp.js
@@ -1,5 +1,6 @@
 
 import TodoList from './TodoList'
+import { useState} from 'react'
 
 
 
@@ -7,16 +8,50 @@ import TodoList from './TodoList'
 const TodoApp = () => {
     
     
+        const [newItem, setNewItem] = useState("");
+        const [items, setItems] = useState([]);
+        
+        
+        function addItem() {
+         const item = {
+           id: Math.floor(Math.random() * 1000),
+           value: newItem,
+           checked: false
+         };
+     
+         
+        
+         setItems(oldList => [...oldList, item]);
+         setNewItem("");
+         
+        }
+        
+        const handleCheck = (id) => {
+         const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+         setItems(listItems);
+         localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+     } 
 
   return (
 
       <div>
      <h1>Todo List</h1>
+     <input 
+     type="text"
+     placeholder='Add an Item...'
+     value={newItem}
+     onChange={e => setNewItem(e.target.value)} />
      
+     <button onClick ={()=> addItem()}>Add</button>
      
      
      <TodoList 
-     
+     items={items}
+     newItem={newItem}
+     handleCheck={handleCheck}
+     setNewItem={setNewItem}
+     addItem={addItem}
+
      />
      
     </div>
--- src/comps/TodoList.js
+++ src/comps/TodoList.js
@@ -1,44 +1,14 @@
-import React, {useState} from 'react'
 
 
 
-
-const TodoList = (props) => {
-    const [newItem, setNewItem] = useState("");
-   const [items, setItems] = useState([]);
+const TodoList = ({newItem, setNewItem, addItem, items, handleCheck}) => {
    
-   
-   function addItem() {
-    const item = {
-      id: Math.floor(Math.random() * 1000),
-      value: newItem,
-      checked: false
-    };
-
-    
-   
-    setItems(oldList => [...oldList, item]);
-    setNewItem("");
-    console.log(item);
-   }
-   
-   const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-}
 
   return (
 
       <div>
           
-     <input 
-     type="text"
-     placeholder='Add an Item...'
-     value={newItem}
-     onChange={e => setNewItem(e.target.value)} />
      
-     <button onClick ={()=> addItem()}>Add</button>
      
      <ul>
       {items.map(item=>{
